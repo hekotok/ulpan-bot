c8fc5389ec4edf634b4c05c2aa14ae97bee24241
hekotok <dan_zhel@mail.ru>
2023-11-16 19:38:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ const {token} = require('./config')
 
 const bot = new TelegramApi(token, { polling: true })
 
-let homework
+const homework = new Map()
 
 const setHomework = async msg => {
     const text = msg.text.replace('/sethomework', '')
     const chatId = msg.chat.id
 
     if (text) {
-        homework = text
+        homework.set(chatId, text)
         await bot.sendMessage(chatId, 'Новая домашняя работа задана')
     }
     else 
         await bot.sendMessage(chatId, 'Вы не добавили домашнее задании, напишите пожалуйста задание после команды /sethomework')
 }
 
-const getHomework = async chatId => await bot.sendMessage(chatId, homework ? homework : 'Походу домашней работы не задали')
+const getHomework = async chatId => await bot.sendMessage(chatId, homework ? homework.get(chatId) : 'Походу домашней работы не задали')
 
 const start = () => {
     bot.setMyCommands([
