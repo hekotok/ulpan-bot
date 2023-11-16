88688eb19932a432c90da5ab6467799f8be35666
hekotok <dan_zhel@mail.ru>
2023-11-16 22:43:13 +0200

    нормальное сохранение дз

--- index.js
+++ index.js
@@ -4,20 +4,36 @@ const {token} = require('./config')
 const bot = new TelegramApi(token, { polling: true })
 
 const homework = new Map()
+const chatTimers = {};
 
-const setHomework = async msg => {
-    const text = msg.text.replace('/sethomework', '')
-    const chatId = msg.chat.id
-
-    if (text) {
-        homework.set(chatId, text)
-        await bot.sendMessage(chatId, 'Новая домашняя работа задана')
-    }
-    else 
-        await bot.sendMessage(chatId, 'Вы не добавили домашнее задании, напишите пожалуйста задание после команды /sethomework')
+const setHomework = async (chatId, msgId) => {
+    homework.set(chatId, msgId)
+    await bot.sendMessage(chatId, 'Новая домашняя работа задана')
 }
 
-const getHomework = async chatId => await bot.sendMessage(chatId, homework ? homework.get(chatId) : 'Походу домашней работы не задали')
+const getHomework = async chatId => await bot.forwardMessage(chatId, chatId, homework.get(chatId))
+
+const waitHomework  = async (chatId, userId) => {
+    bot.sendMessage(chatId, 'Введите домашнее задание.')
+
+    const timer = setTimeout(() => {
+        bot.sendMessage(chatId, 'Время истекло. Установка домашнего задания отменена.');
+        delete chatTimers[chatId];
+    }, 60_000)
+
+    chatTimers[chatId] = timer;
+
+    const listener = async msg => {
+        if (msg.from.id === userId) {
+            await setHomework(chatId, msg.message_id)
+
+            clearTimeout(timer)
+            delete chatTimers[chatId]
+        }
+    }
+
+    bot.on('message', listener);
+}
 
 const start = () => {
     bot.setMyCommands([
@@ -27,16 +43,16 @@ const start = () => {
     ])
 
     bot.on('message', async msg => {
-        const text = msg.text
+        const text = msg.text || msg.caption
         const chatId = msg.chat.id
 
         if (!text)
             return
-    
+
         if (text === '/start')
             await bot.sendMessage(chatId, 'Привет, я бот самой лучшей группы в ульпане')
-        else if (text.includes('/sethomework')) 
-            await setHomework(msg)
+        else if (text === '/sethomework') 
+           await waitHomework(chatId, msg.from.id)
         else if (text === '/gethomework')
             await getHomework(chatId)
     })
