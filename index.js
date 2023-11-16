60f8d24b97b5a5d5be02b67e837ef7ff1fe302c6
hekotok <dan_zhel@mail.ru>
2023-11-16 22:52:21 +0200

    eslint

--- index.js
+++ index.js
@@ -1,61 +1,61 @@
 const TelegramApi = require('node-telegram-bot-api')
-const {token} = require('./config')
+const { token } = require('./config')
 
 const bot = new TelegramApi(token, { polling: true })
 
 const homework = new Map()
-const chatTimers = {};
+const chatTimers = {}
 
 const setHomework = async (chatId, msgId) => {
-    homework.set(chatId, msgId)
-    await bot.sendMessage(chatId, 'Новая домашняя работа задана')
+	homework.set(chatId, msgId)
+	await bot.sendMessage(chatId, 'Новая домашняя работа задана')
 }
 
 const getHomework = async chatId => await bot.forwardMessage(chatId, chatId, homework.get(chatId))
 
-const waitHomework  = async (chatId, userId) => {
-    bot.sendMessage(chatId, 'Введите домашнее задание.')
+const waitHomework = async (chatId, userId) => {
+	bot.sendMessage(chatId, 'Введите домашнее задание.')
 
-    const timer = setTimeout(() => {
-        bot.sendMessage(chatId, 'Время истекло. Установка домашнего задания отменена.');
-        delete chatTimers[chatId];
-    }, 60_000)
+	const timer = setTimeout(() => {
+		bot.sendMessage(chatId, 'Время истекло. Установка домашнего задания отменена.')
+		delete chatTimers[chatId]
+	}, 60_000)
 
-    chatTimers[chatId] = timer;
+	chatTimers[chatId] = timer
 
-    const listener = async msg => {
-        if (msg.from.id === userId) {
-            await setHomework(chatId, msg.message_id)
+	const listener = async msg => {
+		if (msg.from.id === userId) {
+			await setHomework(chatId, msg.message_id)
 
-            clearTimeout(timer)
-            delete chatTimers[chatId]
-        }
-    }
+			clearTimeout(timer)
+			delete chatTimers[chatId]
+		}
+	}
 
-    bot.on('message', listener);
+	bot.on('message', listener)
 }
 
 const start = () => {
-    bot.setMyCommands([
-        { command: '/sethomework', description: 'Укажи домашку' },
-        { command: '/gethomework', description: 'Узнай домашку' },
-        { command: '/translate', description: 'Перевод сообщения' }
-    ])
+	bot.setMyCommands([
+		{ command: '/sethomework', description: 'Укажи домашку' },
+		{ command: '/gethomework', description: 'Узнай домашку' },
+		{ command: '/translate', description: 'Перевод сообщения' }
+	])
 
-    bot.on('message', async msg => {
-        const text = msg.text || msg.caption
-        const chatId = msg.chat.id
+	bot.on('message', async msg => {
+		const text = msg.text || msg.caption
+		const chatId = msg.chat.id
 
-        if (!text)
-            return
+		if (!text)
+			return
 
-        if (text === '/start')
-            await bot.sendMessage(chatId, 'Привет, я бот самой лучшей группы в ульпане')
-        else if (text === '/sethomework') 
-           await waitHomework(chatId, msg.from.id)
-        else if (text === '/gethomework')
-            await getHomework(chatId)
-    })
+		if (text === '/start')
+			await bot.sendMessage(chatId, 'Привет, я бот самой лучшей группы в ульпане')
+		else if (text === '/sethomework')
+			await waitHomework(chatId, msg.from.id)
+		else if (text === '/gethomework')
+			await getHomework(chatId)
+	})
 }
 
 start()
