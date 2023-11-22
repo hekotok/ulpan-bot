8f7b17ecebf6869b359dc545f44f871ddc6a4fad
hekotok <dan_zhel@mail.ru>
2023-11-22 17:37:59 +0200

    правки и более точное расписание

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const waitHomework = async (chatId, userId) => {
 const translateMessage = async (chatId, text) => {
 	const lang = langDefine.franc(text)
 	if (!text)
-		return bot.sendMessage(chatId, 'מצטער, אך לא ציינת טקסט\n\nSorry, but you didn\'t specify any text\n\nИзвините, но вы не указали текст\n\nDésolé, vous n\'avez pas spécifié de texte')
+		return bot.sendMessage(chatId, 'סליחה, לא ציינת את הטקסט. כתוב אותו אחרי הפקודה /translate באותה הודעה\nSorry, you didn\'t specify the text. Write it after the /translate command in the same message\nИзвините, вы не указали текст. Напишите его после команды /translate в том же сообщении\nDésolé, vous n\'avez pas précisé le texte. Écrivez-le après la commande /translate dans le même message')
 	if (![ 'rus', 'eng', 'heb', 'fra' ].includes(lang))
 		return bot.sendMessage(chatId, 'מצטער, אני לא מבין באיזו שפה אתה כותב, נסה לכתוב הודעה מורכבת יותר\n\nSorry, I don\'t understand which language you\'re writing in. Try writing a more detailed message\n\nИзвините, я не понимаю, на каком языке вы пишете. Попробуйте написать более развернутое сообщение\n\nDésolé, je ne comprends pas dans quelle langue vous écrivez. Essayez d\'écrire un message plus détaillé')
 
@@ -65,17 +65,17 @@ const translateMessage = async (chatId, text) => {
 	])).join`\n\n`)
 }
 
-const start = async ({ chat }) => await bot.sendMessage(chat.id, 'שלום, אני בוט של הקבוצה הטובה ביותר באולפן')
+const start = async ({ chat }) => await bot.sendMessage(chat.id, 'שלום, אני בוט של הקבוצה הטובה ביותר באולפן\n\nHello, I am a bot from the best group in the ulpan.\n\nПривет, я бот самой лучшей группы в ульпане.\n\nSalut, je suis un bot du meilleur groupe à l\'oulpan.')
 const help = async ({ chat }) => await bot.sendMessage(chat.id, '/help:\nיספר לך על כל הפקודות שהבוט יכול לבצע\nWill tell you about all the commands that the bot can execute\nРасскажет тебе обо всех командах, которые может выполнить бот\nVous parler de toutes les commandes que le bot peut exécuter\n\n/sethomework:\nיזכור מטלה בית\nWill remember homework\nЗапомнит домашнее задание\nSe souviendra du devoir\n\n/gethomework:\nיספר מה הועמד היום\nWill tell what was assigned today\nРасскажет, что задали на сегодня\nDira ce qui a été assigné aujourd\'hui\n\n/timetable:\nיציג את מערכת השעות של הלימודים באולפן\nWill display the timetable of classes in the ulpan\nПокажет расписание занятий в ульпане\nAffichera l\'horaire des cours à l\'oulpan\n\n/translate:\nהוסף טקסט לאחר הפקודה והבוט יתרגם אותו לעברית, אנגלית, צרפתית ורוסית (הבוט יכול לא להבין הודעה קצרה מאוד או הודעה בשפה אחרת, נסה לכתוב הודעה מורכבת יותר, ונסה לכתוב בעברית במידה רבה)\nAdd text after the command and the bot will translate it to Hebrew, English, French, and Russian (the bot may not understand very short messages or messages in another language, try writing a more complex message, and try to write in Hebrew as much as possible)\nДобавьте текст после команды, и бот переведет его на иврит, английский, французский и русский (бот может не понять очень короткие сообщения или сообщения на других языках, попробуйте написать более сложное сообщение, и старайтесь писать на иврите насколько это возможно)\nAjoutez du texte après la commande et le bot le traduira en hébreu, anglais, français et russe (le bot peut ne pas comprendre les messages très courts ou les messages dans d\'autres langues, essayez d\'écrire un message plus complexe et essayez d\'écrire en hébreu autant que possible)')
 const timetable = async ({ chat }) => await bot.sendPhoto(chat.id, './assets/timetable.png')
 
 const init = () => {
 	bot.setMyCommands([
-		{ command: '/help', description: 'מידע מפורט על צוותים' },
-		{ command: '/sethomework', description: 'הגדרת שאלת בית' },
-		{ command: '/gethomework', description: 'בדוק שאלת בית' },
-		{ command: '/timetable', description: 'בדוק לוח זמנים' },
-		{ command: '/translate', description: 'תרגם הודעה' }
+		{ command: '/help', description: 'Подробная информация о командах' },
+		{ command: '/sethomework', description: 'Установить домашнее задание' },
+		{ command: '/gethomework', description: 'Проверить домашнее задание' },
+		{ command: '/timetable', description: 'Проверить расписание' },
+		{ command: '/translate', description: 'Перевести сообщение' }
 	])
 
 	bot.onText(/\/start/, start)
